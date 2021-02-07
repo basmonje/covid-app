cc48380f01571e172144a6f268c0f3e87df67446
basmonje <basmonje@gmail.com>
2021-02-07 19:02:46 -0300

    modificando informacion

--- src/section/Information/Information.js
+++ src/section/Information/Information.js
@@ -5,18 +5,25 @@ export default function Information() {
   return (
     <div className="information">
       <Container>
-        <p>
-          Los datos se obtienen directamente de{" "}
-          <a href="https://github.com/javierlopeza/datos-coronavirus-chile">
-            javierlopeza/datos-coronavirus-chile
-          </a>
-          . Allí encontrará los scripts que procesan archivos PDF publicados
-          todos los días por el gobierno.
-        </p>
-        <div className="links">
-          <a className="" href="https://github.com/MinCiencia/Datos-COVID19">
-            MinCiencia
-          </a>
+        <div className="text">
+          <h1>Covid-19 en Chile:</h1>
+          <p>
+            Las siguientes gráficas corresponden a las cifras oficiales
+            reportadas por el{" "}
+            <a href="https://github.com/MinCiencia/Datos-COVID19">
+              Ministerio de salud
+            </a>{" "}
+            en sus Reportes Diarios de Covid-19 e Informe Epidemiológico
+            Covid-19.
+          </p>
+          <p>
+            Los datos obtenidos desde el Ministerio de salud son procesadas por{" "}
+            <a href="https://github.com/javierlopeza/datos-coronavirus-chile">
+              javierlopeza/datos-coronavirus-chile
+            </a>
+            . Allí encontrará los scripts que procesan archivos PDF publicados
+            todos los días por el gobierno.
+          </p>
         </div>
       </Container>
       <style jsx>{`
@@ -26,9 +33,14 @@ export default function Information() {
           padding-bottom: 100px;
         }
 
+        .information h1 {
+          margin-bottom: 1.4rem;
+        }
+
         .information p {
           color: #333;
           font-size: 1.4rem;
+          margin-bottom: 1rem;
         }
 
         .information p a {
